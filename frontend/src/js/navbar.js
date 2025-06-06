fc6f44e34591ea839628a6239956b6cbd8677efe
Agustín <managustin01@gmail.com>
2025-06-06 02:42:02 +0000

    chore: make minor UI tweaks

--- frontend/src/js/navbar.js
+++ frontend/src/js/navbar.js
@@ -4,6 +4,8 @@ export function setupMobileNavbar() {
   const menuModal = document.querySelector('#sidebar');
   const menuLinks = menuModal.querySelectorAll("a");
   const navbar = document.querySelector('nav');
+  const body = document.body;
+  let scrollPosition = 0;
 
   btn.addEventListener('click', () => {
       showModal();
@@ -13,30 +15,56 @@ export function setupMobileNavbar() {
       closeModal();
   })
 
+
   function showModal(){
-      menuModal.classList.remove('hidden');
-      menuModal.classList.add('flex', 'justify-center', 'items-center');
-      // Forzamos un reflow para que la transición funcione
-      menuModal.offsetHeight;
-      menuModal.classList.remove('opacity-0');
-      menuModal.classList.add('opacity-100');
+    
+    body.classList.add(`top-[${scrollPosition}px`)
+    body.classList.add('inset-x-0')
+    menuModal.classList.remove('hidden');
+    menuModal.classList.add('flex', 'justify-center', 'items-center');
+    // forzamos un reflow para que la transición funcione
+    menuModal.offsetHeight;
+    menuModal.classList.remove('opacity-0');
+    menuModal.classList.add('opacity-100');
+
+    // Guardar posición actual
+    scrollPosition = window.scrollY;
+    
+  // Bloquear scroll y mantener posición
+    body.style.position = "fixed";
+    body.style.top = `-${scrollPosition}px`;
+    body.style.left = "0";
+    body.style.right = "0";
+    body.classList.add("overflow-hidden");
   }
 
   function closeModal(){
-      menuModal.classList.add('opacity-0');
-      menuModal.classList.remove('opacity-100');
-      
-      // Esperamos a que termine la transición antes de ocultar
-      setTimeout(() => {
-          menuModal.classList.remove('flex');
-          menuModal.classList.add('hidden');
-      }, 300); // 300ms = duración de la transición
+    menuModal.classList.add('opacity-0');
+    menuModal.classList.remove('opacity-100');
+    
+    // Esperamos a que termine la transición antes de ocultar
+    setTimeout(() => {
+        menuModal.classList.remove('flex');
+        menuModal.classList.add('hidden');
+    }, 300); // 300ms = duración de la transición
+
+    
+    // Restaurar scroll
+    body.style.position = "";
+    body.style.top = "";
+    body.style.left = "";
+    body.style.right = "";
+    body.classList.remove("overflow-hidden");    
+    // Volver a la posición donde estaba
+    window.scrollTo({ top: scrollPosition });
   }
 
     menuLinks.forEach(link => {
     link.addEventListener("click", (e) => {
         e.preventDefault(); // evitar salto brusco
 
+        closeModal(); // cerrar el menú modal
+        
         const targetId = link.getAttribute("href");
         const targetEl = document.querySelector(targetId);
 
@@ -46,7 +74,7 @@ export function setupMobileNavbar() {
         });
         }
 
-        closeModal(); // cerrar el menú modal
+        
     });
     });
 
